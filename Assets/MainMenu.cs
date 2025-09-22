1b4a1ca0772f3226165127421126cdb464b8873a
Hoangnhit <hoangnh09022003@gmail.com>
2025-09-22 13:10:23 +0700

    Ui menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync("Asteroids");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!"); 
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; 
+#else
+            Application.Quit(); // Thoát app khi build ra .exe/.apk
+#endif
+    }
+}
